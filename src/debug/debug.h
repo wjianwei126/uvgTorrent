92f285aa6fe80238f6ccb9704c2195a468e27fcc
Simon Bursten <simon.bursten@cortica.com>
2015-07-10 00:33:33 +0300

    tracker object being used

--- src/debug/debug.h
+++ src/debug/debug.h
@@ -8,7 +8,7 @@
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...) fprintf(stderr, KCYN "[DEBUG] %s:%d: " M KNRM "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define debug(M, ...) fprintf(stderr, KCYN "[DEBUG] %s:%d: * " M KNRM "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
