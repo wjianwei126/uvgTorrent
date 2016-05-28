5efae598e159d11d16075987e591a377295a153a
Simon Bursten <smnbursten@gmail.com>
2016-05-28 19:39:24 +0300

    added ut_metadata extended handshake

---
+++ src/peer/packets/peer_extended_handshake_packet.c
@@ -0,0 +1,204 @@
+#include "debug/debug.h"
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include "utils/net/net_utils.h"
+#include "peer/packets/peer_extended_handshake_packet.h"
+
+/* CONNECT REQUEST */
+Peer_Extended_Handshake_Request * Peer_Extended_Handshake_Request_new(size_t size)
+{
+	Peer_Extended_Handshake_Request *req = malloc(size);
+    check_mem(req);
+
+    req->init = Peer_Extended_Handshake_Request_init;
+    req->destroy = Peer_Extended_Handshake_Request_destroy;
+    req->print= Peer_Extended_Handshake_Request_print;
+
+    if(req->init(req) == EXIT_FAILURE) {
+        throw("Peer_Extended_Handshake_Request init failed");
+    } else {
+        // all done, we made an object of any type
+        return req;
+    }
+
+error:
+    if(req) { req->destroy(req); };
+    return NULL;
+}
+
+int Peer_Extended_Handshake_Request_init(Peer_Extended_Handshake_Request *this)
+{
+    char * bencoded_message = "d1:md11:ut_metadatai1eee";
+    
+    uint32_t length = net_utils.htonl(26);
+    uint8_t bt_msg_id = 20;
+    uint8_t handshake_id = 0; 
+
+    int pos = 0;
+    memcpy(&this->bytes[pos], &length, sizeof(uint32_t));
+    pos += sizeof(uint32_t);
+
+    memcpy(&this->bytes[pos], &bt_msg_id, sizeof(uint8_t));
+    pos += sizeof(uint8_t);
+
+    memcpy(&this->bytes[pos], &handshake_id, sizeof(uint8_t));
+    pos += sizeof(uint8_t);
+
+    memcpy(&this->bytes[pos], bencoded_message, strlen(bencoded_message));
+    pos += strlen(bencoded_message);
+
+    debug("POS :: %i", pos);
+	return EXIT_SUCCESS;
+}
+
+void Peer_Extended_Handshake_Request_destroy(Peer_Extended_Handshake_Request *this)
+{
+	if(this) {
+		free(this); 
+	};
+}
+
+void Peer_Extended_Handshake_Request_print(Peer_Extended_Handshake_Request *this)
+{
+
+}
+
+/* CONNECT RESPONSE */
+Peer_Extended_Handshake_Response * Peer_Extended_Handshake_Response_new(size_t size, char raw_response[2048], ssize_t res_size)
+{
+	Peer_Extended_Handshake_Response *req = malloc(size);
+    check_mem(req);
+
+    req->init = Peer_Extended_Handshake_Response_init;
+    req->destroy = Peer_Extended_Handshake_Response_destroy;
+    req->print= Peer_Extended_Handshake_Response_print;
+
+    if(req->init(req, raw_response, res_size) == EXIT_FAILURE) {
+        throw("Peer_Extended_Handshake_Response init failed");
+    } else {
+        // all done, we made an object of any type
+        return req;
+    }
+
+error:
+    if(req) { req->destroy(req); };
+    return NULL;
+}
+
+int Peer_Extended_Handshake_Response_init(Peer_Extended_Handshake_Response *this, char raw_response[2048], ssize_t res_size)
+{
+    debug("EXTENDED RESPONSE :: %s", &raw_response[6]);
+    /*
+	size_t pos = 0;
+
+	memcpy(&this->pstrlen, &raw_response[pos], sizeof(int8_t));
+	pos += sizeof(int8_t);
+
+	this->pstr = calloc(1, 19);
+	memcpy(this->pstr, &raw_response[pos], 19);
+	pos += 19;
+
+	this->peer_id = calloc(1, 20);
+	memcpy(this->peer_id, &raw_response[48], 20);
+	pos += 48;
+
+	//debug("%s", this->pstr);
+	debug("%s", this->peer_id);
+    */
+
+	return EXIT_SUCCESS;
+}
+
+void Peer_Extended_Handshake_Response_destroy(Peer_Extended_Handshake_Response *this)
+{
+	if(this) { 
+		//if(this->pstr) { free(this->pstr); }
+		//if(this->peer_id) { free(this->peer_id); }
+		free(this); 
+	};
+}
+
+void Peer_Extended_Handshake_Response_print(Peer_Extended_Handshake_Response *this)
+{
+
+}
+
+/* CONNECT WRAPPER */
+Peer_Extended_Handshake_Packet * Peer_Extended_Handshake_Packet_new (size_t size)
+{
+	Peer_Extended_Handshake_Packet *conn = malloc(size);
+    check_mem(conn);
+
+    conn->init = Peer_Extended_Handshake_Packet_init;
+    conn->destroy = Peer_Extended_Handshake_Packet_destroy;
+    conn->print = Peer_Extended_Handshake_Packet_print;
+
+    conn->send = Peer_Extended_Handshake_Packet_send;
+    conn->receive = Peer_Extended_Handshake_Packet_receive;
+
+    if(conn->init(conn) == EXIT_FAILURE) {
+        throw("Peer_Extended_Handshake_Packet init failed");
+    } else {
+        return conn;
+    }
+
+error:
+    if(conn) { conn->destroy(conn); };
+    return NULL;
+}
+
+int Peer_Extended_Handshake_Packet_init (Peer_Extended_Handshake_Packet *this)
+{
+	this->request = NULL;
+	this->response = NULL;
+
+	/* prepare request */
+	this->request = NEW(Peer_Extended_Handshake_Request);
+	check_mem(this->request);
+
+	return EXIT_SUCCESS;
+error:
+    return EXIT_FAILURE;
+}
+
+void Peer_Extended_Handshake_Packet_destroy (Peer_Extended_Handshake_Packet *this)
+{
+	if(this){
+		if(this->request != NULL) { this->request->destroy(this->request); };
+		if(this->response != NULL) { this->response->destroy(this->response); };
+		free(this);
+	}
+}
+
+void Peer_Extended_Handshake_Packet_print (Peer_Extended_Handshake_Packet *this)
+{
+
+}
+
+int Peer_Extended_Handshake_Packet_send (Peer_Extended_Handshake_Packet *this, Socket * socket)
+{
+	return socket->send(socket, &this->request->bytes, sizeof(this->request->bytes));
+}
+
+int Peer_Extended_Handshake_Packet_receive (Peer_Extended_Handshake_Packet *this, Socket * socket)
+{
+	char out[2048];
+    ssize_t packet_size = socket->receive(socket, out);
+    
+    if(packet_size > 0){
+    	/* prepare request */
+		this->response = NEW(Peer_Extended_Handshake_Response, out, packet_size);
+
+		check_mem(this->response);
+
+    	return EXIT_SUCCESS;
+    } else {
+    	return EXIT_FAILURE;
+    }
+error:
+	return EXIT_FAILURE;
+}
---
+++ src/peer/packets/peer_extended_handshake_packet.h
@@ -0,0 +1,58 @@
+#ifndef _peer_extended_handshake_packet_h
+#define _peer_extended_handshake_packet_h
+
+#include "macro/macro.h"
+#include "utils/sock/socket.h"
+
+typedef struct Peer_Extended_Handshake_Request Peer_Extended_Handshake_Request;
+struct Peer_Extended_Handshake_Request { 
+	int (*init) (Peer_Extended_Handshake_Request *this);
+    void (*print) (Peer_Extended_Handshake_Request *this);
+    void (*destroy) (Peer_Extended_Handshake_Request *this);
+
+	char bytes[30];
+};
+
+Peer_Extended_Handshake_Request *Peer_Extended_Handshake_Request_new (size_t size);
+int Peer_Extended_Handshake_Request_init (Peer_Extended_Handshake_Request *this);
+void Peer_Extended_Handshake_Request_destroy (Peer_Extended_Handshake_Request *this);
+void Peer_Extended_Handshake_Request_print (Peer_Extended_Handshake_Request *this);
+
+/* PEER HANDSHAKE RESPONSE */
+typedef struct Peer_Extended_Handshake_Response Peer_Extended_Handshake_Response;
+struct Peer_Extended_Handshake_Response { 
+	int (*init) (Peer_Extended_Handshake_Response *this, char raw_response[2048], ssize_t res_size);
+    void (*print) (Peer_Extended_Handshake_Response *this);
+    void (*destroy) (Peer_Extended_Handshake_Response *this);
+
+    int metadata_size;
+};
+
+Peer_Extended_Handshake_Response *Peer_Extended_Handshake_Response_new (size_t size, char raw_response[2048], ssize_t res_size);
+int Peer_Extended_Handshake_Response_init (Peer_Extended_Handshake_Response *this, char raw_response[2048], ssize_t res_size);
+void Peer_Extended_Handshake_Response_destroy (Peer_Extended_Handshake_Response *this);
+void Peer_Extended_Handshake_Response_print (Peer_Extended_Handshake_Response *this);
+
+/* PEER HANDSHAKE WRAPPER */
+typedef struct Peer_Extended_Handshake_Packet Peer_Extended_Handshake_Packet;
+struct Peer_Extended_Handshake_Packet {
+	int (*init) (Peer_Extended_Handshake_Packet *this);
+    void (*print) (Peer_Extended_Handshake_Packet *this);
+    void (*destroy) (Peer_Extended_Handshake_Packet *this);
+
+	int (*send) (Peer_Extended_Handshake_Packet *this, Socket * socket);
+	int (*receive) (Peer_Extended_Handshake_Packet *this, Socket * socket);
+
+	Peer_Extended_Handshake_Request * request;
+	Peer_Extended_Handshake_Response * response;
+};
+
+Peer_Extended_Handshake_Packet * Peer_Extended_Handshake_Packet_new (size_t size);
+int Peer_Extended_Handshake_Packet_init (Peer_Extended_Handshake_Packet *this);
+void Peer_Extended_Handshake_Packet_destroy (Peer_Extended_Handshake_Packet *this);
+void Peer_Extended_Handshake_Packet_print (Peer_Extended_Handshake_Packet *this);
+
+int Peer_Extended_Handshake_Packet_send (Peer_Extended_Handshake_Packet *this, Socket * socket);
+int Peer_Extended_Handshake_Packet_receive (Peer_Extended_Handshake_Packet *this, Socket * socket);
+
+#endif
